24aaa31da800f54af2a7fea8181df4210a35d5d3
takeru-a <pypy20191022@gmail.com>
2021-12-22 22:59:26 +0900

    コードの削減

--- expanding.js
+++ expanding.js
@@ -10,35 +10,40 @@ let check = () =>{
 }
 
 
-let adclose1 = () => {
-    let tag = document.getElementById('p1');
+let adclose = (p) => {
+    let tag = document.getElementById(p);
     tag.style.display='none';
     cnt++;
     console.log(cnt);
     check();
 };
-let adclose2 = () => {
-    let tag = document.getElementById('p2');
-    tag.style.display='none';
-    cnt++;
-    console.log(cnt);
-    check();
+
+let expand = (btn, picture) =>{
+    let button = document.getElementById(btn);
+    let pc = document.getElementById(picture);
+    button.style.transform='translate(-20px,-20px)';
+    pc.style.transform="scale(1.5,1.5)";
+    setTimeout(function() {
+        button.style.transform='translate(0px,0px)';
+        pc.style.transform="scale(1,1)";
+      }, 500);
 };
-let adclose3 = () => {
-    let tag = document.getElementById('p3');
-    tag.style.display='none';
-    cnt++;
-    console.log(cnt);
-    check();
-};
+
 let button1 = document.getElementById('b1');
 let button2 = document.getElementById('b2');
 let button3 = document.getElementById('b3');
 
-button1.addEventListener('click',adclose1);
-button2.addEventListener('click',adclose2);
-button3.addEventListener('click',adclose3);
-
-//button1.onclick = adclose1();
-//button2.onclick = adclose2();
-//button3.onclick = adclose3();
+button1.addEventListener('click',function(){
+    adclose('p1');
+});
+let btn2 = 'b2'
+let picture2 = 'pc2';
+button2.addEventListener('mouseover',function(){
+    expand(btn2,picture2);
+});
+button2.addEventListener('click',function(){
+    adclose('p2');
+});
+button3.addEventListener('click',function(){
+    adclose('p3')
+});
