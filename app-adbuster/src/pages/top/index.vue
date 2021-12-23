5ce3b5f3dfe9fddf89915496d83b39866db96ac0
Kouta-fd <kotafd0124@icloud.com>
2021-12-23 22:26:56 +0900

    add:vuecli

---
+++ app-adbuster/src/pages/top/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Header></Header>
+    <h1>テスト</h1>
+  </div>
+</template>
+<script>
+import Header from "../../components/Header"
+export default {
+   name: 'top',
+   components: {
+     Header
+   }
+}
+</script>
