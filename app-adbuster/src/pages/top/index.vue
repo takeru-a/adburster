af9f4fea442f44c27395e2ebfc606208bd3a273c
teraomoyu <dmntc.5299@gmail.com>
2021-12-23 23:37:34 +0900

    add:少し動くゲームの追加

--- app-adbuster/src/pages/top/index.vue
+++ app-adbuster/src/pages/top/index.vue
@@ -3,6 +3,8 @@
     <Header></Header>
     <h1>テスト</h1>
     <router-link to="/usage">使い方</router-link>
+    <br>
+    <router-link to="/game/slide">ゲーム</router-link>
   </div>
 </template>
 <script>
