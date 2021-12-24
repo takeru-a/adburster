506bd34f0940aa872d72cd20169e8b4af49ff704
teraomoyu <dmntc.5299@gmail.com>
2021-12-24 10:53:14 +0900

    add:浮かび上がる広告追加

---
+++ app-adbuster/src/components/game/EmergeAd.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="ad" id="ad">
+        <img class="ad-img" src="../../assets/imgs/ad-example.gif">
+        <button class="close-btn" id="close-btn" type="button" @click="btnClick()"></button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'slidead',
+    methods: {
+        btnClick(){
+            var ad = document.getElementById("ad");
+            ad.remove();
+        }
+    }
+}
+</script>
+
+<style scoped> 
+    .ad{
+        position: relative;
+        width: 70%;
+        margin-left: auto;
+        margin-right: auto;
+        opacity: 0;
+        transform: translate(0px, 100px);
+        transition: all cubic-bezier(0.215, 0.61, 0.355, 1) 3s;
+    }
+
+    .ad:hover{
+        transform: translate(0, 0);
+        transition: all cubic-bezier(0.215, 0.61, 0.355, 1) 3s;
+        opacity: 1;
+    }
+
+    .ad-img{
+        position: absolute;
+        width: 100%;
+        height: 200px;
+    }
+
+    .close-btn{
+        position: absolute;
+        top: 0px;
+        right: 0px;
+        background-color: transparent;
+        border: none;
+    }
+
+    .close-btn:hover{
+        color: red;
+    }
+
+    .close-btn:after{
+        font-family: "fontello";
+        content: '\e800';
+    }
+
+    .checkbox{
+        height: 50%;
+    }
+
+    @font-face {
+        font-family: 'fontello';
+        src: url('../../assets/fonts/fontello.eot?22568919');
+        src: url('../../assets/fonts/fontello.eot?22568919#iefix') format('embedded-opentype'),
+            url('../../assets/fonts/fontello.woff2?22568919') format('woff2'),
+            url('../../assets/fonts/fontello.woff?22568919') format('woff'),
+            url('../../assets/fonts/fontello.ttf?22568919') format('truetype'),
+            url('../../assets/fonts/fontello.svg?22568919#fontello') format('svg');
+        font-weight: normal;
+        font-style: normal;
+    }
+</style>
--- app-adbuster/src/pages/top/index.vue
+++ app-adbuster/src/pages/top/index.vue
@@ -4,7 +4,9 @@
     <h1>テスト</h1>
     <router-link to="/usage">使い方</router-link>
     <br>
-    <router-link to="/game/slide">ゲーム</router-link>
+    <router-link to="/game/slide">ゲーム1(少し動く)</router-link>
+    <br>
+    <router-link to="/game/emerge">ゲーム2(浮かび上がる)</router-link>
   </div>
 </template>
 <script>
