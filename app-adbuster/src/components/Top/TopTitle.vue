4a172ce7411c53ca59bf56cbb2643a86d857e2c2
Shin.Yamamoto <yamashin0123@outlook.jp>
2021-12-25 17:27:21 +0900

    add:ホーム画面

---
+++ app-adbuster/src/components/Top/TopTitle.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+      <div class="wrapper">
+        <img class="newImg" src="../../assets/imgs/new.svg" alt="">
+        <h1 class="topTitle">アドバスター</h1>
+        <img src="../../assets/imgs/subTitle.svg" alt="">
+      </div>
+    </div>
+</template>
+<script>
+export default {
+  name: "toptitle",
+  components: {
+    
+  },
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+  }
+};
+</script>
+<style scoped>
+  .wrapper {
+    margin: 0 auto;
+    width: 730px;
+    height: 200px;
+    margin-bottom: 100px;
+  }
+  .topTitle {
+    position: relative;
+    margin: 0;
+    letter-spacing: 0.1em;
+    font-size: 110px;
+    font-weight: 700;
+    color: #DA0000;
+        text-shadow:
+        7px  0 0 #000,
+        7px 7px 0 #000,
+        0 7px 0 #000,
+        -7px 7px 0 #000,
+        -7px 0 0 #000,
+        -7px -7px 0 #000,
+        0 -7px 0 #000,
+        7px -7px 0 #000;
+  }
+  .newImg {
+    position: absolute;
+    top: 10%;
+    right: 40%;
+  }
+</style>
