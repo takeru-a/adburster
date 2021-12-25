c2f15679393fd22bde9561a2984bee9e341f68ff
Shin.Yamamoto <yamashin0123@outlook.jp>
2021-12-25 23:44:40 +0900

    add:playscreen追加　fix:ホーム画面のスタイル修正

--- app-adbuster/src/components/Top/TopTitle.vue
+++ app-adbuster/src/components/Top/TopTitle.vue
@@ -3,7 +3,7 @@
       <div class="wrapper">
         <img class="newImg" src="../../assets/imgs/new.svg" alt="">
         <h1 class="topTitle">アドバスター</h1>
-        <img src="../../assets/imgs/subTitle.svg" alt="">
+        <img width="80%" src="../../assets/imgs/subTitle.svg" alt="">
       </div>
     </div>
 </template>
@@ -26,14 +26,15 @@ export default {
   .wrapper {
     margin: 0 auto;
     width: 730px;
-    height: 200px;
-    margin-bottom: 100px;
+    margin-bottom: 2%;
+    text-align: center;
   }
   .topTitle {
-    position: relative;
-    margin: 0;
+    position: absolute;
+    top: 19%;
+    margin: 0 100px; 
     letter-spacing: 0.1em;
-    font-size: 110px;
+    font-size: 90px;
     font-weight: 700;
     color: #DA0000;
         text-shadow:
@@ -48,7 +49,8 @@ export default {
   }
   .newImg {
     position: absolute;
-    top: 10%;
-    right: 40%;
+    top: 12%;
+    right: 29%;
+    width: 8%;
   }
 </style>
